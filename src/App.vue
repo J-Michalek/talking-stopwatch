d580851adecb2df7f5029db76a4df7c008eb38b0
J-Michalek <jaku.michalek@gmail.com>
2024-03-21 19:20:31 +0100

    feat(voice): use speech synthesis

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,63 @@
 <script setup lang="ts">
-import { useInterval } from '@vueuse/core';
+import { useInterval } from "@vueuse/core";
+import { computed, ref } from "vue";
 
+const { counter, pause, reset, resume } = useInterval(1000, {
+  controls: true,
+  immediate: false,
+  callback(count) {
+    speak(count);
+  },
+});
 
+const voices = ref<SpeechSynthesisVoice[]>([]);
+const selectedVoice = ref<SpeechSynthesisVoice | null>(null);
 
+speechSynthesis.onvoiceschanged = () => {
+  const v = speechSynthesis.getVoices();
+  if (v.length > 0) {
+    voices.value = speechSynthesis.getVoices();
+    selectedVoice.value = voices.value[0];
+  }
+};
 
+const modelVoice = computed({
+  get() {
+    return selectedVoice.value?.name ?? "";
+  },
+  set(name: string) {
+    selectedVoice.value =
+      voices.value.find((voice) => voice.name === name) ?? null;
+  },
+});
 
-const {
-  counter,
-  pause,
-  reset,
-  resume,
-} = useInterval(1000, {controls: true, immediate: false, callback(count) {
-    playAudio(`src/assets/sounds/en_num_${count < 10 ? `0${count}` : count}.mp3`)
-},})
+function speak(count: number): void {
+  const utterance = new SpeechSynthesisUtterance(String(count));
 
-let audio: HTMLAudioElement | null = null
-function playAudio(filePath: string): void {
-if (audio) {
-  audio.pause()
-  audio.remove()
+  utterance.voice = selectedVoice.value;
+  utterance.rate = 2;
+  speechSynthesis.cancel();
+  speechSynthesis.speak(utterance);
 }
-   audio = new Audio(filePath)
-   
-  audio.addEventListener("ended", (): void => {
-    audio?.remove()
-  })
-  audio.play()
-
-}
-
 </script>
 
 <template>
- <div class="flex justify-center flex-col">
-
-<h1 class="text-xl">{{ counter }}</h1>
-<div class="flex gap-3">
-  <button @click="resume">Start</button>
-  <button @click="pause">Stop</button>
-  <button @click="reset">Restart</button>
-  
-</div>
- </div>
+  <div class="flex justify-center flex-col">
+    <select v-if="selectedVoice != null" v-model="modelVoice" name="voices">
+      <option v-for="voice of voices" :key="voice.name" :value="voice.name">
+        {{ voice.name }}
+      </option>
+    </select>
+    <h1 class="text-xl">{{ counter }}</h1>
+    <div class="flex gap-3">
+      <button @click="resume">Start</button>
+      <button @click="pause">Stop</button>
+      <button @click="reset">Restart</button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
-
 .flex {
   display: flex;
 }
